7d5882f9d8f7e943df5e8a8ccb2d06b8c95025ab
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-10-09 10:24:47 -0500

    Cards to show information created as isolated component

---
+++ src/components/UI/InformationCards/Card/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+
+function Card(){
+  return (
+    <div className="characterCard">
+      <h1>Nombre del personaje</h1>
+      <p>Mundo del personaje</p>
+      <p>Informacion relevante del personaje</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/UI/InformationCards/InformationCards.js
@@ -0,0 +1,17 @@
+import Card from "./Card/Card";
+import "./InformationCards.css"
+
+function InformacionCards() {
+  return (
+    <section className="charactersSection">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </section>
+  );
+}
+
+export default InformacionCards;
