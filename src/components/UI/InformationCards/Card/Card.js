79daffbfaf97ee264f9ef05a3876a38992a970cd
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-10-12 00:21:46 -0500

    Created the content card to show character info when the user is on hover

--- src/components/UI/InformationCards/Card/Card.js
+++ src/components/UI/InformationCards/Card/Card.js
@@ -1,13 +1,23 @@
 import './Card.css'
 
 function Card(props){
-  const {title, image} = props;
+  console.log("🚀 ~ file: Card.js ~ line 4 ~ Card ~ props", props)
+  const {name, image, species, status, type, gender} = props.characterInfo;
 
   return (
     <div className="characterCard">
       <img src={image} alt='Character avatar'/>
       <div className="contentBox">
-        <h2>{title}</h2>
+        <h2>{name}</h2>
+      </div>
+      <div className='contentDetails'>
+        <h3>Character Information</h3>
+        <ol>
+          <li>Specie: {species}</li>
+          <li>Status: {status}</li>
+          <li>Type: {type || 'unknown'}</li>
+          <li>Gender: {gender}</li>
+        </ol>
       </div>
     </div>
   )
