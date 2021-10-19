1c4f406bfcf4028cbe45aeec7d3eb68edfda8f63
Juan Felipe Montaña Perdomo <juanmontana1398@gmail.com>
2021-10-18 21:22:23 -0500

    Routes and linking configuration completed for manage 3 screens, prettier applied to code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,33 @@
-import './Navbar.css'
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
-function NavBar(){
-  return(
-    <nav className='navBar'>
-      <div className='rightSection'>
+function NavBar() {
+  return (
+    <nav className="navBar">
+      <div className="rightSection">
         <h2>Rick and morty API</h2>
       </div>
       <div>
         <ul>
-          <li> <a href="#">Characters</a> </li>
-          <li> <a href="#">Episodes</a> </li>
-          <li> <a href="#">Universes</a> </li>
+          <li>
+            <NavLink to="/" activeClassName="active" exact>
+              Characters
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/episodes" activeClassName="active">
+              Episodes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/locations" activeClassName="active">
+              Locations
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
 export default NavBar;
