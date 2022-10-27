de088e98aff0242432a60c8feddc44d3f83490cb
Geraldine <geralchavez@hotmail.com>
2022-10-26 23:14:37 -0500

    Rick and Morty API project. Initially the project was entirely created in a single component to display the character information on cards, a style file was created. Verify that there are images that are not displayed completely filling their div.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './styles.css'
+
+
+
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect( () => {
+    fetch(`https://rickandmortyapi.com/api/character
+    `)
+    .then ( (response) => response.json())
+    .then ( ({results}) => {
+      setData(results);
+      console.log(results);
+    })
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="box">
+        {data && data.map(({id, name, status, image, species, gender, location, episode}) => (
+            <div className="card" key={id}>
+              <div className="container">
+
+                <div className="image">
+                  <img  className="charImg" src={image} />
+                </div>
+
+                <div className="details">
+                  <h2>#{id} {name}</h2>
+                  <h3>{status}-{species}-{gender}</h3>
+                  <p>Last known location:</p>
+                  <strong>{location.name}</strong>
+                  <p>First seen in:</p>
+                  <p>{episode[0]}</p>
+
+                </div>
+
+              </div>
+
+            </div>
+
+        ))}
+        </div>
+      </>
   );
 }
 
